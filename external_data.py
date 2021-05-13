23d70a705bd3aee2e99af89129666d51b9cea650
Ethan Zammit <ethanzammit@gmail.com>
2021-05-14 00:12:21 +0200

    added council data

---
+++ external_data.py
@@ -0,0 +1,61 @@
+import json
+import os
+
+_parsed_entries = None
+
+
+def read_unesco_file(path=os.path.join('resources', 'jsonified_unesco_data.json')):
+    entries = {}
+    with open(path,encoding="utf-8") as json_file:
+        data = json.load(json_file)
+        for entry in data['row']:            
+            entry_info = {}
+            entry_info['title'] = entry['site']
+            # entry_info['country'] = entry['states']
+            entry_info['short_desc'] = f"A {entry['category'].lower()} site in {entry['location']}"
+            entry_info['long_desc'] = entry['short_description']
+            entry_info['lat'] = float(entry['latitude'])
+            entry_info['lon'] = float(entry['longitude'])
+            entry_info['imageurl'] = entry['image_url']
+            # entry_info['rad'] = 20
+
+            entries[(entry_info['lat'], entry_info['lon'])] = entry_info
+    return entries
+
+
+def read_council_file(path=os.path.join('resources', 'jsonified_unesco_data.json')):
+    entries = {}
+    data = []
+    with open(path,encoding="utf-8") as json_file:
+        data = json.load(json_file)
+    for entry in data:
+        entry_info = {}
+
+        entry_info['lat'] = float(entry["Geolocation Coordinates"].split(',')[0])
+        entry_info['lon'] = float(entry["Geolocation Coordinates"].split(',')[1])
+        entry_info['title'] = entry["Heritage Site Description"]
+        entry_info['short_desc'] = f"A {entry['Type'].lower()} found in {entry['Location'].lower()}"
+        entry_info['long_desc']  = entry_info['short_desc']
+        entry_info['imageurl'] = ''
+
+        entries[(entry_info['lat'], entry_info['lon'])] = entry_info
+    return entries
+
+        
+
+def getData():
+    global _parsed_entries
+    if _parsed_entries is None:
+        _parsed_entries = read_unesco_file()
+        _parsed_entries.update(read_council_file)
+
+    return _parsed_entries
+
+
+
+if __name__ == '__main__':
+    entries = getData()
+    for coords,data in entries.items():
+        if "malta" in data['country'].lower():
+            print(data['title'])
+            print(f'Key is {coords}')
